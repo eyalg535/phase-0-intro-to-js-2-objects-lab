164af6ce54d55159f9156b12b1fa396f735251a9
eyal gerby <eyalg535@gmail.com>
2022-04-22 12:21:17 -0400

    commit tree 0a3612dd4babc82a9de4ca331595693c8f378044

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: "Eyal",
+    streetAddress: "6544 Saunders",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employees = { ...employee };
+    employees[key] = value;
+    return employees;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    delete newEmployee.key;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    employee[key] = value;
+    delete employee.key;
+    return employee;
+}
